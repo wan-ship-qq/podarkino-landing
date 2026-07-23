508d7fe7c46cc6e8aa26a321e4f3f4a8f2e3aeb2
wan-ship-qq <>
2026-07-23 15:16:23 +0000

    Add and optimize customer review images

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-const assetVersion = "20260723-1455";
+const assetVersion = "20260723-1515";
 
 function versionedAsset(source) {
   if (typeof source !== "string" || !source.startsWith("assets/")) return source;
